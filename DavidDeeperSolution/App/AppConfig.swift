d3141e0f45a8b11ab007834386b8b41ea10bb809
Deividas Janušas <deividas.j@kryptis.lt>
2025-11-27 00:10:15 +0200

    Add googleMapsToken to AppConfig

--- DavidDeeperSolution/App/AppConfig.swift
+++ DavidDeeperSolution/App/AppConfig.swift
@@ -3,4 +3,5 @@ import Foundation
 enum AppConfig {
     static let baseURL = URL(string: "https://bathus.staging.deeper.eu/api/")!
     static let bundleIdentifier = Bundle.main.bundleIdentifier ?? "com.task.DavidDeeperSolution"
+    static let googleMapsToken = "" // TODO: - Add your token
 }
